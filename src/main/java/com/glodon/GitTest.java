960921306b2fe5c7bef4139473edf8fe08ef3228
闫博元 <yanby-a@glodon.com>
2022-11-10 14:10:05 +0800

    First Commit

---
+++ src/main/java/com/glodon/GitTest.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2020-2022, Glodon Digital Supplier & Purchaser BU.
+ * <p>
+ * All Rights Reserved.
+ */
+package com.glodon;
+
+/**
+ * 类描述
+ *
+ * @author 闫博元
+ * @date 2022-11-10 11:47:53
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello,Git!");
+	}
+}
